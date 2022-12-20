d81f3c6354e22337f0498df3098f8da928196242
Gael <demondragong@gmail.com>
2022-12-20 12:54:49 +0100

    edit content

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,105 +1,120 @@
-import { FC } from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/react'; 
+import { FC } from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 // Import Styled Components
-import { FooterContainer, FlexWrap } from './Footer.atoms';
-import Link from 'next/link';
+import { FooterContainer, FlexWrap } from "./Footer.atoms";
+import Link from "next/link";
 
 const SocialLink: FC<{ url: string; imgSrc: string; altText: string }> = ({
-    url,
-    imgSrc,
-    altText,
+  url,
+  imgSrc,
+  altText,
 }) => {
-    const style = {
-        border: 'none',
-    };
+  const style = {
+    border: "none",
+  };
 
-    return (
-        <Link href={url} passHref>
-            <a
-                css={{
-                    border: 'none',
-                }}
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                <img
-                    css={{
-                        width: '24px',
-                        height: '24px',
-                    }}
-                    width='24px'
-                    height='24px'
-                    src={imgSrc}
-                    alt={altText}
-                />
-            </a>
-        </Link>
-    );
+  return (
+    <Link href={url} passHref>
+      <a
+        css={{
+          border: "none",
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          css={{
+            width: "24px",
+            height: "24px",
+          }}
+          width="24px"
+          height="24px"
+          src={imgSrc}
+          alt={altText}
+        />
+      </a>
+    </Link>
+  );
 };
 
-const CopyNotice = styled.div(() => css({
-    fontSize: '0.75em',
-    margin: '30px 0 20px 0',
-    textAlign: 'center',
-}));
+const CopyNotice = styled.div(() =>
+  css({
+    fontSize: "0.75em",
+    margin: "30px 0 20px 0",
+    textAlign: "center",
+  })
+);
 
+const Title = styled.div(() =>
+  css({
+    color: "#fafafa",
+    fontSize: "2.5em",
+    fontWeight: "lighter",
+  })
+);
 
-const Title = styled.div(() => css({
-    color: '#fafafa',
-    fontSize: '2.5em',
-    fontWeight: 'lighter',
-}));
+const FooterTitle = styled.div(() =>
+  css({
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    paddingBottom: "20px",
+  })
+);
 
-const FooterTitle = styled.div(() => css({
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    paddingBottom: '20px',
-}));
+const SocialWrapper = styled.div(() =>
+  css({
+    display: "flex",
+    justifyContent: "center",
+    padding: "15px 0",
 
-const SocialWrapper = styled.div(() => css({
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '15px 0',
-
-    ['a']: {
-        display: 'block',
-        margin: '0 10px',
+    ["a"]: {
+      display: "block",
+      margin: "0 10px",
     },
-}));
+  })
+);
 
 const Footer = () => (
-    <>
-        <FooterContainer>
-            <FlexWrap>
-                <FooterTitle>
-                    <div css={{
-                        marginRight: '10px',
-                    }}>
-                        <img height="60px" width="60px" src='https://static.killedbygoogle.com/com/tombstone-alt.svg' alt="Tombstone" />
-                    </div>
-                    <Title>Death by AI</Title>
-                </FooterTitle>
-                <div>
-                    <p>
-                        Death by AI is a RIP-off of <a href="https://killedbygoogle.com/">Killed by Google</a>, an open-source
-                        list of discontinued Google products and services.
-                        Go check their page! It's literally the same UI.
-                        </p><p>Except this page focuses on things that AI is threatening.
+  <>
+    <FooterContainer>
+      <FlexWrap>
+        <FooterTitle>
+          <div
+            css={{
+              marginRight: "10px",
+            }}
+          >
+            <img
+              height="60px"
+              width="60px"
+              src="https://static.killedbygoogle.com/com/tombstone-alt.svg"
+              alt="Tombstone"
+            />
+          </div>
+          <Title>Death by AI</Title>
+        </FooterTitle>
+        <div>
+          <p>
+            Death by AI is a RIP-off of{" "}
+            <a href="https://killedbygoogle.com/">Killed by Google</a>, an
+            open-source list of discontinued Google products and services. Go
+            check their page! It's literally the same UI.
           </p>
-                    
-                </div>
-                <SocialWrapper>
-                    <SocialLink
-                        url="https://github.com/demondragong/deathbyai"
-                        altText="GitHub"
-                        imgSrc='https://static.killedbygoogle.com/com/github.svg'
-                    />
-                </SocialWrapper>
-            </FlexWrap>
-        </FooterContainer>
-    </>
+          <p>Except this page focuses on things that AI is threatening.</p>
+          <p>If you know someone or something that is at risk because of AI please let us know <a href="https://github.com/demondragong/deathbyai">here</a>.</p>
+        </div>
+        <SocialWrapper>
+          <SocialLink
+            url="https://github.com/demondragong/deathbyai"
+            altText="GitHub"
+            imgSrc="https://static.killedbygoogle.com/com/github.svg"
+          />
+        </SocialWrapper>
+      </FlexWrap>
+    </FooterContainer>
+  </>
 );
 export default Footer;
