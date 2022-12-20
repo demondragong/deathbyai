ea4791705c2f08077edd423fc7b127c9423bff3c
Gael <demondragong@gmail.com>
2022-12-20 16:48:53 +0100

    add possibility to not display link

--- components/List/Item.tsx
+++ components/List/Item.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
+import React from "react";
+import dynamic from "next/dynamic";
 import {
   format,
   formatDistance,
   parseISO,
   formatDistanceToNow,
-} from 'date-fns';
+} from "date-fns";
 
-import ExIcon from '../../assets/ex.svg'
-import EwIcon from '../../assets/ew.svg'
-import CrIcon from '../../assets/cr.svg'
-import EnIcon from '../../assets/en.svg'
-import VuIcon from '../../assets/vu.svg'
-import NtIcon from '../../assets/nt.svg'
+import ExIcon from "../../assets/ex.svg";
+import EwIcon from "../../assets/ew.svg";
+import CrIcon from "../../assets/cr.svg";
+import EnIcon from "../../assets/en.svg";
+import VuIcon from "../../assets/vu.svg";
+import NtIcon from "../../assets/nt.svg";
 
-import { ProductWithSlug } from '../../types/Product';
-import Badge from '../Badge'; 
+import { ProductWithSlug } from "../../types/Product";
+import Badge from "../Badge";
 
 // Import Styled Components
 import {
@@ -26,16 +26,18 @@ import {
   Icon,
   IconContainer,
   ListItem,
-} from './Item.atoms';
+} from "./Item.atoms";
 
 export default function Item(props: ProductWithSlug) {
-
   const isPast = () => {
     return new Date() > new Date(props.dateClose);
   };
 
   const getYears = () => {
-    const duration = formatDistance(parseISO(props.dateClose), parseISO(props.dateOpen));
+    const duration = formatDistance(
+      parseISO(props.dateClose),
+      parseISO(props.dateOpen)
+    );
 
     if (!isPast()) {
       return ` It will be ${duration} old.`;
@@ -46,49 +48,46 @@ export default function Item(props: ProductWithSlug) {
   const getIcon = () => {
     switch (props.conservationStatus) {
       case "ex":
-        return <ExIcon />
+        return <ExIcon />;
       case "ew":
-        return <EwIcon />
+        return <EwIcon />;
       case "cr":
-        return <CrIcon />
+        return <CrIcon />;
       case "en":
-        return <EnIcon />
+        return <EnIcon />;
       case "vu":
-        return <VuIcon />
+        return <VuIcon />;
       case "nt":
-        return <NtIcon />
+        return <NtIcon />;
       default:
         break;
     }
   };
-
 
   const conservationStatus = () => {
-    
     switch (props.conservationStatus) {
       case "ex":
-        return <ConservationStatus>Extinct</ConservationStatus>
+        return <ConservationStatus>Extinct</ConservationStatus>;
         break;
       case "ew":
-        return <ConservationStatus>Extinct in the Wild</ConservationStatus>
+        return <ConservationStatus>Extinct in the Wild</ConservationStatus>;
         break;
       case "cr":
-        return <ConservationStatus>Critically Endangered</ConservationStatus>
+        return <ConservationStatus>Critically Endangered</ConservationStatus>;
         break;
       case "en":
-        return <ConservationStatus>Endangered</ConservationStatus>
+        return <ConservationStatus>Endangered</ConservationStatus>;
         break;
       case "vu":
-        return <ConservationStatus>Vulnerable</ConservationStatus>
+        return <ConservationStatus>Vulnerable</ConservationStatus>;
         break;
       case "nt":
-        return <ConservationStatus>Near Threatened</ConservationStatus>
+        return <ConservationStatus>Near Threatened</ConservationStatus>;
         break;
       default:
         break;
     }
   };
-
 
   return (
     <ListItem>
@@ -99,18 +98,15 @@ export default function Item(props: ProductWithSlug) {
       </IconContainer>
       <ContentContainer>
         <h2>
-          <a href={props.link} target="_blank" rel="noopener noreferrer">
-            {props.name}
-          </a>
+          {props.link ? (
+            <a href={props.link} target="_blank" rel="noopener noreferrer">{props.name}</a>
+          ) : (
+            props.name
+          )}
         </h2>
-        <Description>
-          {props.description}
-        </Description>
-        <Hope>
-          {props.hope}
-        </Hope>
+        <Description>{props.description}</Description>
+        <Hope>{props.hope}</Hope>
       </ContentContainer>
     </ListItem>
   );
 }
-
