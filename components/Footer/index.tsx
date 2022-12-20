84ee53b20c3689d817e0888ec0ba95f3f9d41e18
Gael <demondragong@gmail.com>
2022-12-20 09:28:21 +0100

    fix typo

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -86,7 +86,7 @@ const Footer = () => (
                     <p>
                         Death by AI is a RIP-off of <a href="https://killedbygoogle.com/">Killed by Google</a>, an open-source
                         list of discontinued Google products and services.
-                        Go check their page! It's litteraly the same UI.
+                        Go check their page! It's litterally the same UI.
                         </p><p>Except this page focuses on things that AI is threatening.
           </p>
                     
