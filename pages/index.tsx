0b532e7808edcc9c253cc4164afed61669ad4bea
Gael <demondragong@gmail.com>
2022-12-22 10:01:00 +0100

    update meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,16 +19,16 @@ const HomePage: React.FC<{ items: ProductWithSlug[] }> = ({ items }) => {
             <Head>
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5" />
-                <meta name="description" content="Death by AI is like Killed by Google but two words have been changed. A RIP-off!" />
+                <meta name="description" content="Thinking about a career change? Look no further! Our list of professions, apps, and human activities that are ripe for replacement by artificial intelligence will have you feeling confident about your future job security. Come see the full list and find out if your job is at risk. Don't say we didn't warn you." />
                 <link rel="shortcut icon" href="favicon.png" />
                 <title>Death by AI</title>
                 <meta name="theme-color" content="#FAFAFA" />
                 <link rel="canonical" href="https://deathbyai.com" />
                 <meta itemProp="name" content="Death by AI" />
-                <meta itemProp="description" content="Death by AI is like Killed by Google but two words have been changed. A RIP-off!" />
+                <meta itemProp="description" content="Thinking about a career change? Look no further! Our list of professions, apps, and human activities that are ripe for replacement by artificial intelligence will have you feeling confident about your future job security. Come see the full list and find out if your job is at risk. Don't say we didn't warn you." />
                 <meta name="og:title" property="og:title" content="Death by AI" />
                 <meta name="og:url" property="og:url" content="https://deathbyai.com" />
-                <meta name="og:description" property="og:description" content="Death by AI is like Killed by Google but two words have been changed. A RIP-off!" />
+                <meta name="og:description" property="og:description" content="Thinking about a career change? Look no further! Our list of professions, apps, and human activities that are ripe for replacement by artificial intelligence will have you feeling confident about your future job security. Come see the full list and find out if your job is at risk. Don't say we didn't warn you." />
                 <meta name="og:site_name" property="og:site_name" content="Death by AI" />
                 <meta name="og:type" property="og:type" content="website" />
             </Head>
